93408871a25aa75b9bbea80c60f9206bfec462e3
Aleksander Khanteev <khaale@yandex.ru>
2019-07-31 23:56:08 +0400

    Add annotation target type.

--- spring-cadence/src/main/java/org/springframework/cadence/annotation/CadenceActivities.java
+++ spring-cadence/src/main/java/org/springframework/cadence/annotation/CadenceActivities.java
@@ -1,12 +1,15 @@
 package org.springframework.cadence.annotation;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Scope("singleton")
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CadenceActivities {
   String worker();
--- spring-cadence/src/main/java/org/springframework/cadence/annotation/CadenceWorkflow.java
+++ spring-cadence/src/main/java/org/springframework/cadence/annotation/CadenceWorkflow.java
@@ -1,12 +1,15 @@
 package org.springframework.cadence.annotation;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Scope("prototype")
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CadenceWorkflow {
   String worker();
